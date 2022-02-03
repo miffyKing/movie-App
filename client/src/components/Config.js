fd1d8cfeec6626a7a1bcc3dbce896e444060fca0
조병화 <chobyounghwa@jobyeonghwaui-MacBookAir.local>
2022-02-03 23:22:39 +0900

    lec 4

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,5 +1,9 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '3b15bb25f54fdda01555dcd11c458aa1'
 
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const IMAGE_BASE_URL =  'http://image.tmdb.org/t/p/'
+
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
